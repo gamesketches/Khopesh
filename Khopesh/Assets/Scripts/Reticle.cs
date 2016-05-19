516fdbb1f1b0c5f6d499ee55eecc624d7587fdbc
gamesketches <sam.von.ehren@gmail.com>
2016-05-18 22:24:56 -0400

    Melee attacks clearing bullets causes hitsparks

--- Khopesh/Assets/Scripts/Reticle.cs
+++ Khopesh/Assets/Scripts/Reticle.cs
@@ -34,6 +34,9 @@ public class Reticle : MonoBehaviour {
 					sparks.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(hitSparkSpritePath);	
 					return;
 				} else if(collider.gameObject.tag == "Hippo" || collider.gameObject.tag == "Crane" || collider.gameObject.tag == "Gator") {
+					string hitSparkSpritePath = string.Concat("sprites/hitSparks/hit", color == Color.blue ? "BR" : "RB");
+					GameObject sparks = (GameObject)Instantiate(Resources.Load<GameObject>("prefabs/HitSparks"), transform.position, Quaternion.identity);
+					sparks.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(hitSparkSpritePath);	
 					Destroy(collider.gameObject);
 				}
 			}
