d4e3f0dff4867803a0f4c8adfbaffc2d7b255137
gamesketches <sam.von.ehren@gmail.com>
2016-05-18 21:36:50 -0400

    Fixed Burst shots not doing damage

--- Khopesh/Assets/Scripts/InputManager.cs
+++ Khopesh/Assets/Scripts/InputManager.cs
@@ -202,7 +202,7 @@ public class InputManager : MonoBehaviour {
 			}
 		}
 		for(int i = 0; i < bulletAngles.Count; i++) {
-			BulletDepot.Bullet bullet = new BulletDepot.Bullet();
+			BulletDepot.Bullet bullet = bullets.types.projectileTypes[(int)bulletTypes[i]].volleys[0].volley[0];
 			bullet.angle = (int)bulletAngles[i];
 			CreateBullet(bullet, bulletTypes[i]);
 		}
